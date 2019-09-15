a6845b264c6ae9b9b9a099e0fd45e96edbe0cf34
Toluen <tranducan.bkhcm11@gmail.com>
2019-09-15 08:44:48 +0700

    sdsadasd

--- WindowsFormsApplication1/WindowsFormsApplication1/Class/DBUtils.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Class/DBUtils.cs
@@ -23,9 +23,9 @@ namespace WindowsFormsApplication1
             //string username = "SQLUSER";
             //string password = "12345";
 
-            string datasource = "172.16.0.12";
-            string database = "ERPSOFT";
-            string username = "ERPUSER";
+            string datasource = @"DESKTOP-83OVHRC\SQLEXPRESS";
+            string database = "TLSOFT";
+            string username = "SQLUSER";
             string password = "12345";
 
 
@@ -35,19 +35,19 @@ namespace WindowsFormsApplication1
         public static SqlConnection GetERPDBConnection()
         {
             //Data Source = LONG; Initial Catalog = TEST; Integrated Security = True
-            string datasource = "172.16.0.11";
-            string database = "TECHLINK";
-            string username = "soft";
-            string password = "techlink@!@#";
+            //string datasource = @"DESKTOP-83OVHRC\SQLEXPRESS";
+            //string database = "TECHLINK";
+            //string username = "soft";
+            //string password = "techlink@!@#";
 
             //string datasource = "Long";
             //string database = "TECHLINK";
             //string username = "SQLUSER";
             //string password = "12345";
-            //string datasource = @"DESKTOP-83OVHRC\SQLEXPRESS";
-            //string database = "TECHLINK";
-            //string username = "SQLUSER";
-            //string password = "12345";
+            string datasource = @"DESKTOP-83OVHRC\SQLEXPRESS";
+            string database = "TECHLINK";
+            string username = "SQLUSER";
+            string password = "12345";
 
 
             return DBSQLServerUtils.GetERPDBConnection(datasource, database, username, password);
--- WindowsFormsApplication1/WindowsFormsApplication1/ERPShowOrder/ERPShowMain.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/ERPShowOrder/ERPShowMain.Designer.cs
@@ -45,6 +45,8 @@
             this.shippingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.productionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.materialToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trv_dept = new System.Windows.Forms.TreeView();
+            this.lbl_dept = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_show)).BeginInit();
             this.menuStrip1.SuspendLayout();
@@ -52,6 +54,8 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.btn_toExcel);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.cmd_COPTC_TC002);
@@ -62,23 +66,19 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.cmd_COPTC_TC001);
             this.groupBox1.Controls.Add(this.btn_search);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(0, 90);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Location = new System.Drawing.Point(0, 87);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(1362, 131);
+            this.groupBox1.Size = new System.Drawing.Size(1059, 82);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Processing";
             // 
             // btn_toExcel
             // 
-            this.btn_toExcel.Location = new System.Drawing.Point(1168, 16);
-            this.btn_toExcel.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_toExcel.Location = new System.Drawing.Point(908, 12);
             this.btn_toExcel.Name = "btn_toExcel";
-            this.btn_toExcel.Size = new System.Drawing.Size(147, 65);
+            this.btn_toExcel.Size = new System.Drawing.Size(114, 50);
             this.btn_toExcel.TabIndex = 27;
             this.btn_toExcel.Text = "To Excel";
             this.btn_toExcel.UseVisualStyleBackColor = true;
@@ -88,10 +88,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(242, 20);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(188, 15);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 26;
             this.label2.Text = "Order No:";
             // 
@@ -99,21 +98,19 @@
             // 
             this.cmd_COPTC_TC002.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmd_COPTC_TC002.FormattingEnabled = true;
-            this.cmd_COPTC_TC002.Location = new System.Drawing.Point(247, 41);
-            this.cmd_COPTC_TC002.Margin = new System.Windows.Forms.Padding(4);
+            this.cmd_COPTC_TC002.Location = new System.Drawing.Point(192, 31);
             this.cmd_COPTC_TC002.MaxLength = 8;
             this.cmd_COPTC_TC002.Name = "cmd_COPTC_TC002";
-            this.cmd_COPTC_TC002.Size = new System.Drawing.Size(154, 25);
+            this.cmd_COPTC_TC002.Size = new System.Drawing.Size(121, 21);
             this.cmd_COPTC_TC002.TabIndex = 25;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(451, 17);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(351, 13);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(113, 17);
+            this.label4.Size = new System.Drawing.Size(85, 13);
             this.label4.TabIndex = 24;
             this.label4.Text = "Date Time From:";
             // 
@@ -121,10 +118,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(683, 16);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(531, 12);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(98, 17);
+            this.label3.Size = new System.Drawing.Size(75, 13);
             this.label3.TabIndex = 23;
             this.label3.Text = "Date Time To:";
             // 
@@ -133,10 +129,9 @@
             this.dtp_from.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtp_from.CustomFormat = "yyyy-MM-dd";
             this.dtp_from.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtp_from.Location = new System.Drawing.Point(455, 42);
-            this.dtp_from.Margin = new System.Windows.Forms.Padding(4);
+            this.dtp_from.Location = new System.Drawing.Point(354, 32);
             this.dtp_from.Name = "dtp_from";
-            this.dtp_from.Size = new System.Drawing.Size(196, 23);
+            this.dtp_from.Size = new System.Drawing.Size(153, 20);
             this.dtp_from.TabIndex = 22;
             // 
             // dtp_to
@@ -144,20 +139,18 @@
             this.dtp_to.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtp_to.CustomFormat = "yyyy-MM-dd";
             this.dtp_to.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtp_to.Location = new System.Drawing.Point(687, 42);
-            this.dtp_to.Margin = new System.Windows.Forms.Padding(4);
+            this.dtp_to.Location = new System.Drawing.Point(534, 32);
             this.dtp_to.Name = "dtp_to";
-            this.dtp_to.Size = new System.Drawing.Size(196, 23);
+            this.dtp_to.Size = new System.Drawing.Size(153, 20);
             this.dtp_to.TabIndex = 21;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(54, 21);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(42, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.Size = new System.Drawing.Size(90, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Department Code";
             // 
@@ -165,19 +158,17 @@
             // 
             this.cmd_COPTC_TC001.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmd_COPTC_TC001.FormattingEnabled = true;
-            this.cmd_COPTC_TC001.Location = new System.Drawing.Point(37, 41);
-            this.cmd_COPTC_TC001.Margin = new System.Windows.Forms.Padding(4);
+            this.cmd_COPTC_TC001.Location = new System.Drawing.Point(29, 31);
             this.cmd_COPTC_TC001.Name = "cmd_COPTC_TC001";
-            this.cmd_COPTC_TC001.Size = new System.Drawing.Size(154, 25);
+            this.cmd_COPTC_TC001.Size = new System.Drawing.Size(121, 21);
             this.cmd_COPTC_TC001.TabIndex = 1;
             this.cmd_COPTC_TC001.SelectedIndexChanged += new System.EventHandler(this.cmd_MOCTA_TA001_SelectedIndexChanged);
             // 
             // btn_search
             // 
-            this.btn_search.Location = new System.Drawing.Point(994, 16);
-            this.btn_search.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_search.Location = new System.Drawing.Point(773, 12);
             this.btn_search.Name = "btn_search";
-            this.btn_search.Size = new System.Drawing.Size(147, 65);
+            this.btn_search.Size = new System.Drawing.Size(114, 50);
             this.btn_search.TabIndex = 0;
             this.btn_search.Text = "Search";
             this.btn_search.UseVisualStyleBackColor = true;
@@ -185,14 +176,15 @@
             // 
             // dgv_show
             // 
+            this.dgv_show.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgv_show.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_show.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.dgv_show.Location = new System.Drawing.Point(0, 229);
-            this.dgv_show.Margin = new System.Windows.Forms.Padding(4);
+            this.dgv_show.Location = new System.Drawing.Point(119, 172);
             this.dgv_show.Name = "dgv_show";
             this.dgv_show.ReadOnly = true;
             this.dgv_show.RowHeadersWidth = 51;
-            this.dgv_show.Size = new System.Drawing.Size(1362, 740);
+            this.dgv_show.Size = new System.Drawing.Size(940, 401);
             this.dgv_show.TabIndex = 5;
             this.dgv_show.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_show_CellDoubleClick);
             // 
@@ -203,8 +195,8 @@
             this.tmenu_window});
             this.menuStrip1.Location = new System.Drawing.Point(0, 60);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(6, 3, 0, 3);
-            this.menuStrip1.Size = new System.Drawing.Size(1362, 30);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1059, 24);
             this.menuStrip1.TabIndex = 6;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -215,46 +207,66 @@
             this.productionToolStripMenuItem,
             this.materialToolStripMenuItem});
             this.tmenu_window.Name = "tmenu_window";
-            this.tmenu_window.Size = new System.Drawing.Size(119, 24);
+            this.tmenu_window.Size = new System.Drawing.Size(97, 20);
             this.tmenu_window.Text = "Menu Window";
             // 
             // shippingToolStripMenuItem
             // 
             this.shippingToolStripMenuItem.Name = "shippingToolStripMenuItem";
-            this.shippingToolStripMenuItem.Size = new System.Drawing.Size(164, 26);
+            this.shippingToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
             this.shippingToolStripMenuItem.Text = "Shipping";
             this.shippingToolStripMenuItem.Click += new System.EventHandler(this.ShippingToolStripMenuItem_Click);
             // 
             // productionToolStripMenuItem
             // 
             this.productionToolStripMenuItem.Name = "productionToolStripMenuItem";
-            this.productionToolStripMenuItem.Size = new System.Drawing.Size(164, 26);
+            this.productionToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
             this.productionToolStripMenuItem.Text = "Production";
             this.productionToolStripMenuItem.Click += new System.EventHandler(this.ProductionToolStripMenuItem_Click);
             // 
             // materialToolStripMenuItem
             // 
             this.materialToolStripMenuItem.Name = "materialToolStripMenuItem";
-            this.materialToolStripMenuItem.Size = new System.Drawing.Size(164, 26);
+            this.materialToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
             this.materialToolStripMenuItem.Text = "Material";
             this.materialToolStripMenuItem.Click += new System.EventHandler(this.MaterialToolStripMenuItem_Click);
             // 
+            // trv_dept
+            // 
+            this.trv_dept.CheckBoxes = true;
+            this.trv_dept.Location = new System.Drawing.Point(0, 199);
+            this.trv_dept.Name = "trv_dept";
+            this.trv_dept.Size = new System.Drawing.Size(121, 374);
+            this.trv_dept.TabIndex = 7;
+            // 
+            // lbl_dept
+            // 
+            this.lbl_dept.AutoSize = true;
+            this.lbl_dept.Location = new System.Drawing.Point(0, 176);
+            this.lbl_dept.Name = "lbl_dept";
+            this.lbl_dept.Size = new System.Drawing.Size(72, 13);
+            this.lbl_dept.TabIndex = 8;
+            this.lbl_dept.Text = "Department";
+            // 
             // ERPShowMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1362, 969);
+            this.ClientSize = new System.Drawing.Size(1059, 573);
+            this.Controls.Add(this.lbl_dept);
+            this.Controls.Add(this.trv_dept);
             this.Controls.Add(this.dgv_show);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ERPShowMain";
             this.Text = "ERPShowMain";
             this.Load += new System.EventHandler(this.ERPShowMain_Load);
             this.Controls.SetChildIndex(this.menuStrip1, 0);
             this.Controls.SetChildIndex(this.groupBox1, 0);
             this.Controls.SetChildIndex(this.dgv_show, 0);
+            this.Controls.SetChildIndex(this.trv_dept, 0);
+            this.Controls.SetChildIndex(this.lbl_dept, 0);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_show)).EndInit();
@@ -284,5 +296,7 @@
         private System.Windows.Forms.ToolStripMenuItem productionToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem materialToolStripMenuItem;
         private System.Windows.Forms.Button btn_toExcel;
+        private System.Windows.Forms.TreeView trv_dept;
+        private System.Windows.Forms.Label lbl_dept;
     }
 }
